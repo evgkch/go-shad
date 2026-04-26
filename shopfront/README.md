dcb65334a190964520e395a90976f49eeef254d0
jzuss <evg.kch@gmail.com>
2026-04-26 18:46:33 +0300

    shopfront

--- shopfront/README.md
+++ shopfront/README.md
@@ -25,5 +25,5 @@ sudo apt install redis-server
 Комментарии по запуску бд в docker смотрите в задаче [dao](../dao/).
 
 ```
-(cd shopfront && docker compose up -d --wait && env REDIS="localhost:6379" go test -v ./... -count=1 || true && docker compose down)
+(cd shopfront && docker compose up -d --wait && env REDIS="127.0.0.1:6379" go test -v ./... -count=1 || true && docker compose down)
 ```
--- shopfront/shopfront.go
+++ shopfront/shopfront.go
@@ -2,8 +2,71 @@
 
 package shopfront
 
-import "github.com/go-redis/redis/v8"
+import (
+	"context"
+	"fmt"
+
+	"github.com/go-redis/redis/v8"
+)
+
+type counters struct {
+	rdb *redis.Client
+}
 
 func New(rdb *redis.Client) Counters {
-	panic("not implemented")
+	return &counters{rdb: rdb}
+}
+
+func (c *counters) GetItems(ctx context.Context, ids []ItemID, userID UserID) ([]Item, error) {
+	items := make([]Item, len(ids))
+	countCmds := make([]*redis.StringCmd, len(ids))
+	viewedCmds := make([]*redis.BoolCmd, len(ids))
+
+	_, err := c.rdb.Pipelined(ctx, func(p redis.Pipeliner) error {
+		for i, id := range ids {
+			countCmds[i] = p.Get(ctx, countKey(id))
+			viewedCmds[i] = p.SIsMember(ctx, viewedKey(id), int64(userID))
+		}
+
+		return nil
+	})
+	if err != nil && err != redis.Nil {
+		return nil, err
+	}
+
+	for i := range ids {
+		viewCount, err := countCmds[i].Int64()
+		if err != nil && err != redis.Nil {
+			return nil, err
+		}
+		viewed, err := viewedCmds[i].Result()
+		if err != nil {
+			return nil, err
+		}
+
+		items[i] = Item{
+			ViewCount: int(viewCount),
+			Viewed:    viewed,
+		}
+	}
+
+	return items, nil
+}
+
+func (c *counters) RecordView(ctx context.Context, id ItemID, userID UserID) error {
+	_, err := c.rdb.Pipelined(ctx, func(p redis.Pipeliner) error {
+		p.Incr(ctx, countKey(id))
+		p.SAdd(ctx, viewedKey(id), int64(userID))
+		return nil
+	})
+
+	return err
+}
+
+func countKey(id ItemID) string {
+	return fmt.Sprintf("shopfront:item:%d:count", id)
+}
+
+func viewedKey(id ItemID) string {
+	return fmt.Sprintf("shopfront:item:%d:viewed", id)
 }
